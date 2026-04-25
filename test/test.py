dbb6f495b739da580ef7f1b9cf65e564520fa587
varsha-1101 <varsharamesh1101@gmail.com>
2026-04-25 11:23:53 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,34 +7,44 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
+async def test_full_adder(dut):
+    dut._log.info("Starting Gate-level Hardend simulation")
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await Timer(50,unit="ns")
     dut.rst_n.value = 1
+    await Timer(50,unit="ns")
+    
+    test_cases = [
+        (0,0,0,0,0),
+        (1,1,0,0,1),
+        (1,1,1,1,1),
+        (1,0,0,1,0)
+    ]
 
-    dut._log.info("Test project behavior")
+     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    for a,b,c,e_sum,e_cout in test_cases:
+    dut.ui_in.value = (c << 2) | (b << 1) | a
+    
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await Timer(20,units="ns")
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+   try:
+       output_val = int(dut.uo_out.value)
+       actual_sum =output_val & 1
+       actual_cout =(output_val >> 1) & 1
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    assert actual_sum == e_sum, f"Sum Error: A={a} B={b} C={c}"
+    assert actual_cout == e_cout,f"Cout Error: A={a} B={b} C={c}"
+    dut._log.info(f"Input: {a},{b},{c} -> Sum: {actual_sum}, Carry: {actual_carry} [PASS]")
+
+except ValueError:
+dut._log.error(f"Logic error: uo_out is {str(dut.uo_out.value)}"]
+raise
